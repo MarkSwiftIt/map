a1f4359b8d55195ba263ff734e02d419f1f687a4
MarkSwiftIt <markgocharovit@gmail.com>
2022-07-20 16:36:03 +0300

    Initial Commit

---
+++ map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by Mark Goncharov on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
